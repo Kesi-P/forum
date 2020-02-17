0b8b3c0eaeb6f9417ba308678d48a7241c52bd33
Kesi Promnual <kesi.promnual@myzone.org>
2020-02-17 12:48:16 +0000

    find the discussion using slug instead of id

--- resources/views/discussions/index.blade.php
+++ resources/views/discussions/index.blade.php
@@ -6,11 +6,19 @@
 @foreach($discussions as $discussion)
 <div class="card">
   <div class="card-header">
-    <img src="{{ Gravatar::src($discussion->sus->email) }}" alt="">
-    {{ $discussion->title}}
+    <div class="d-flex justify-content-between">
+      <div >
+        <img width="40px" height="40px" style="border-radius: 50%" src="{{ Gravatar::src($discussion->sus->email) }}" alt="">
+        <span><small>{{ $discussion->sus->name}}</small></span>
+      </div>
+      <div>
+        <a href="{{ route('discussion.show' ,$discussion->slug)}}" class="btn btn-success btn-sm">View</a>
+      </div>
+    </div>
   </div>
 
   <div class="card-body">
+      <b>{{ $discussion->title}}</b><br>
     {!! $discussion->content !!}
   </div>
 </div>
